2c55b2eca88b1f2e5fc80c078574657eec64ad94
Khaled alhilal <khaledalhilal2002@gmail.com>
2024-08-20 13:38:39 +0300

    writing jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import JSX from './Lect1-jsx';
 
 function App() {
+  const divStyle = {
+    padding: '10px',
+    margin: '10px',
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={divStyle}>
+      <JSX />
     </div>
   );
 }
---
+++ src/Lect1-jsx.js
@@ -0,0 +1,7 @@
+export default function JSX() {
+    return (
+        <div style={{ backgroundColor: 'red', color: 'white' }}>
+            Here is the JSX
+        </div>
+    );
+}
